c9bfe6e3791ff7246459306f79c3d9f46b25cb64
ryanjduffy <technisode@gmail.com>
2011-07-07 00:07:30 -0500

    initial admob port and new timer control.  also directory cleanup

--- example/depends.js
+++
@@ -1,5 +0,0 @@
-enyo.depends(
-	"example.js",
-	"../src/calendar-picker/Calendar.js",
-	"css/calendar-picker.css"
-);
---
+++ src/depends.js
@@ -0,0 +1,7 @@
+enyo.depends(
+	"example.js",
+	"calendar-picker/Calendar.js",
+	"calendar-picker/calendar-picker.css",
+	"admob/admob.js",
+	"timer/timer.js"
+);
